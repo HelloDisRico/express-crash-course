fa1cb96a4a93b1e4614bb3fd051e4ed91e653553
Kevin Rico <krico315@gmail.com>
2022-11-07 10:17:16 -0700

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+// const exp = require('constants');
 const express = require('express');
+const path = require('path');
 
+
+const logger = require('./MiddleWare/logger')
 const app = express();
 
+//Init MiddleWare
+//                            app.use(logger);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-});
+// Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false } ));
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'))
+
+//app.get('/', (req, res) => {
+  //  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000; 
 
